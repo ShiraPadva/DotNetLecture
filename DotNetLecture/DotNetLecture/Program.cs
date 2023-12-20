3af33569d7c719feb359e2bad72f0abd4e063035
ShiraPadva <shira.70000@gmail.com>
2023-12-20 19:21:28 +0200

    Add delegate callback example

--- DotNetLecture/DotNetLecture/Program.cs
+++ DotNetLecture/DotNetLecture/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using DotNetLecture;
+using System;
 
 class Program
 {
     public static int Main()
     {
         Console.WriteLine("Hello world");
+
+        DelegateDemo.DelegateMain();
 
         return 0;
     }
