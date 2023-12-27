259d3e171244844885c3b4bd24081e486060ae71
ShiraPadva <shira.70000@gmail.com>
2023-12-27 18:09:36 +0200

    Add simple delegate demo

---
+++ DotNetLecture/DotNetLecture/DelegateDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLecture
+{
+    internal class DelegateDemo
+    {
+        // Delegate Declaration
+        delegate int MathOperation(int a, int b);
+        public static void Demo() 
+        {
+            // Methods to be Used with the Delegate
+            int Add(int a, int b) => a + b;
+            int Subtract(int a, int b) => a - b;
+
+            // Delegate Instances
+            MathOperation addDelegate = Add;
+            MathOperation subtractDelegate = Subtract;
+
+            // Using Delegates
+            int resultAdd = addDelegate(5, 3);       // resultAdd = 8
+            int resultSubtract = subtractDelegate(8, 3);  // resultSubtract = 5
+
+            // Built-In delegates:
+
+            // Action Delegate (No return value)
+            Action<string> greetAction = (name) => Console.WriteLine($"Hello, {name}!");
+
+            // Func Delegate (With return value)
+            Func<int, int, int> multiplyFunc = (x, y) => x * y;
+
+            // Using Action and Func
+            greetAction("Alice");           // Prints: Hello, Alice!
+            int resultMultiply = multiplyFunc(4, 5);  // resultMultiply = 20
+
+        }
+    }
+}
